d8e97d25732868747819f2f6dab7a5d5b8b8d1c6
Robin Monjo <robinmonjo@gmail.com>
2012-12-01 23:27:44 +0100

    Initial Commit

---
+++ MonTonTon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MonTonTon
+//
+//  Created by Robin Monjo on 12/1/12.
+//  Copyright (c) 2012 Robin Monjo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
